81265b7e6357e8f796b4b3c47dd2ecf2aa02942d
AndreyShvachko <an.shvachko@gmail.com>
2024-08-14 17:50:40 +0300

    validation method implemented

--- src/managers/note_manager.py
+++ src/managers/note_manager.py
@@ -9,28 +9,34 @@ from models.note import Note  # Assume Note class is defined in 'note.py'
 class NoteManager:
     def __init__(self) -> None:
         self.notes: List[Note] = []
+ 
+    def validate_note(self, note: Note) -> bool:
+        """
+        Validates the note's title and content
 
+        """
+        if not note.title or len(note.title) < 5: #Number 5 is for example. Could be any number
+            print("Error: The title must not be empty and should have at least 5 characters.")
+            return False
+        if not note.content:
+            print("Error: The content must not be empty.")
+            return False
+        return True
     def add_note(self, note: Note) -> None:
         """
         Adds a new note to the list of notes.
         """
-        #validate title
-        if not note.title:
-            raise ValueError ("The title cannot be empty.")
-        
-        if len(note.title) <5: #minimum length of the title. can be any number.
-            raise ValueError ("Title must be at least 5 characters long.")
-        
-        #Validation of content
-        if not note.content:
-            raise ValueError ("The content cannot be empty.")
-        
-        #Add note to the list is validation successful
+        #Check if the note with the same name already exists
+        for existing_note in self.notes:
+            if existing_note.title.lower() == note.title.lower():
+                print(f"Error: A note with the same '{note.title}' already exists")
+                return
+            
+        #Add the new note to the list
         self.notes.append(note)
-        print(f"Note titled '{note.title}' successfully added.")
+        print(f"Success: Note titled '{note.title}' successfully added.")
 
-        raise NotImplementedError("The 'add_note' method is not implemented.")
-
+   
     def search_notes(self, query: str) -> List[Note]:
         """
         Searches for notes by title or content and returns a list of matching notes.
