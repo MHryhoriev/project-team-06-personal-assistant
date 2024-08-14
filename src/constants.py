51ecacd4a448a69645ff4d89455f09f94feb32ce
TSCRM\m.hryhoriev <m.hryhoriev@creatio.com>
2024-08-14 18:15:48 +0300

    PA-24: Storing data on the hard disk & PA-25: Saving data when the assistant restarts

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+"""
+This module contains constants used throughout the application.
+"""
+
+# File paths
+CONTACT_DATA_FILE_PATH = 'data/contacts_data.json'
+NOTE_DATA_FILE_PATH = 'data/note_data.json'
