f7930b89a3fe99a74662027746e92393dd076858
AlexnderDoroshenko <doroshenkoaldm@gmail.com>
2024-08-15 20:23:58 +0300

    Added search and sort Notes by tags

--- src/managers/note_manager.py
+++ src/managers/note_manager.py
@@ -41,3 +41,51 @@ class NoteManager:
         Deletes a note with the specified note_id.
         """
         raise NotImplementedError("The 'delete_note' method is not implemented.")
+
+    def search_by_tag(self, tag: str) -> List[Note]:
+        """
+        Search the Note by it`s tags.
+
+        Args:
+            tag (str): A tag name to search Notes.
+            
+        Returns:
+            List[Note]: A list of notes that contain the specified tag.
+
+        """
+        return [note for note in self.notes if tag in note.tags]
+
+    def sort_notes_by_tags(self, order: str = 'asc') -> List[Note]:
+        """
+        Sort the notes by the number of tags and then alphabetically by tags.
+        
+        The primary sorting is by the number of tags. If there are multiple notes
+        with the same number of tags, they are then sorted alphabetically based
+        on the tags. The sorting order is determined by the `order` parameter.
+
+        Args:
+            order (str): The order of sorting. Must be either 'asc' for ascending
+            or 'desc' for descending. Default is 'asc'.
+
+        Returns:
+            List[Note]: A list of notes sorted by the specified criteria.
+
+        Raises:
+            ValueError: If `order` is not 'asc' or 'desc'.
+        """
+        if order not in ('asc', 'desc'):
+            raise ValueError("Order must be 'asc' or 'desc'")
+        
+        if order == 'asc':
+            sorted_notes = sorted(
+                self.notes, 
+                key=lambda note: (len(note.tags), sorted(note.tags))
+            )
+        else:
+            sorted_notes = sorted(
+                self.notes, 
+                key=lambda note: (len(note.tags), sorted(note.tags)),
+                reverse=True
+            )
+        
+        return sorted_notes
