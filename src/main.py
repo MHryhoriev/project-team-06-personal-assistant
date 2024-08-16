35c45d4ff42c4e55ed4dd0692c2425ac321a8399
TSCRM\m.hryhoriev <m.hryhoriev@creatio.com>
2024-08-16 14:39:20 +0300

    PA-29: Add all the necessary commands and handlers

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from managers import ContactManager, NoteManager
-from utils import parse_input, handle_add_contact, handle_search_contact, handle_show_all_notes
+from utils import (
+    parse_input,
+    handle_add_contact,
+    handle_search_contact,
+    handle_show_all_notes,
+    handle_remove_contact,
+    handle_show_all_contacts,
+    handle_upcoming_birthdays,
+    handle_edit_contact
+)
 from storage import ContactStorage, NoteStorage
 from constants import CONTACT_DATA_FILE_PATH, NOTE_DATA_FILE_PATH
 from prompt_toolkit import PromptSession
@@ -24,22 +33,24 @@ def main():
 
     while True:
         try:
-            user_input = session.prompt("Enter a command (add/search/remove/exit): ")
+            user_input = session.prompt("Enter a command (add_contact/remove_contact/search_contact/all_contacts/all_notes/check_birthdays): ")
             command, *args = parse_input(user_input)
 
             if command in ["exit", "close"]:
                 print("Good bye!")
                 break
-            elif command == "add":
+            elif command == "add_contact":
                 handle_add_contact(contact_manager)
-            elif command == "remove":
-                pass
-            elif command == "search":
+            elif command == "remove_contact":
+                handle_remove_contact(contact_manager)
+            elif command == "search_contact":
                 handle_search_contact(contact_manager)
-            elif command == "all":
-                pass
+            elif command == "all_contacts":
+                handle_show_all_contacts(contact_manager)
             elif command == "all_notes":
                 handle_show_all_notes(note_manager)
+            elif command == "check_birthdays":
+                handle_upcoming_birthdays(contact_manager)
             else:
                 suggest_command(user_input)
 
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,12 @@
 from .command_parser import parse_input
-from .command_handlers import handle_add_contact, handle_search_contact, handle_show_all_notes
+from .command_handlers import (
+    handle_add_contact,
+    handle_search_contact,
+    handle_show_all_notes,
+    handle_remove_contact,
+    handle_show_all_contacts,
+    handle_upcoming_birthdays,
+    handle_edit_contact
+)
 from .custom_decorators import error_handler
 from .suggestion_utils import suggest_command, completer
